43504eeb6664ac7d4a2b2ffe124007aa0232197e
jainvishal520 <vjtinu.jain431@gmail.com>
2019-04-21 17:08:21 +0530

    project component

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return(
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+                <span className="card-title">Project Titile</span>
+                <p>Posted by vishal jain</p>
+                <p className="grey-text">21st April</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectSummary;
